f25fc4a585f99b515105cd510a2e4c4f7aca4fdf
rupesh-dev30 <cgcrupesh@gmail.com>
2025-05-06 15:52:46 +0530

    login and signup functionalites

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function Home() {  
   return <div className="h-screen flex items-center justify-center gap-4">
     <Button>Primary</Button>
     <Button variant="secondary">Secondary</Button>

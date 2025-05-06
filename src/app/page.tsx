0faaa3d1f883f4a29d3a7fde34950b73910a54c8
rupesh-dev30 <cgcrupesh@gmail.com>
2025-05-06 16:31:29 +0530

    logout and custome session middleware appwrite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {  
-  return <div className="h-screen flex items-center justify-center gap-4">
-    <Button>Primary</Button>
-    <Button variant="secondary">Secondary</Button>
-    <Button variant="destructive">Destructive</Button>
-    <Button variant="outline">Outline</Button>
-    <Button variant="muted">Muted</Button>
-    <Button variant="ghost">Ghost</Button>
-    <Button variant="teritary">Teritary</Button>
-  </div>;
+import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/useCurrent";
+import { useLogout } from "@/features/auth/api/useLogout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if(!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[data])
+
+  return (
+    <div className="h-screen flex items-center justify-center gap-4">
+      ONLY VISIBLE TO AUTHORIZED USER
+
+      <Button onClick={() => mutate()}>Logout</Button>
+    </div>
+  );
 }
